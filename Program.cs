85f992deff8513d13ce76f1a9b7508b5e4cc8b1e
Blaise <blazzze0517@gmail.com>
2024-03-11 21:49:51 -0400

    Pushing as-is code to Github

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int result = 0;
+
+            string answer;
+
+            bool conditionsMet = false;
+
+            Console.WriteLine("Hello, welcome to the calculator program!");
+            Console.Write("Please enter first number: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter your second number: ");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do?: ");
+
+            Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication, or d for division.");
+
+            answer = Console.ReadLine();
+
+            while (!conditionsMet)
+            {
+
+                if (answer == "a")
+                {
+                    result = num1 + num2;
+                    conditionsMet = true;
+                }
+                else if (answer == "s")
+                {
+                    result = num1 - num2;
+                    conditionsMet = true;
+                }
+                else if (answer == "m")
+                {
+                    result = num1 * num2;
+                    conditionsMet = true;
+                }
+                else if (answer == "d")
+                {
+                    result = num1 / num2;
+                    conditionsMet = true;
+                }
+                else
+                {
+                    Console.WriteLine("Silly sod, enter either a for addition, s for subtraction, m for multiplication, or d for division");
+                    answer = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("The result is: " + result);
+
+            Console.WriteLine("Thank you for using the calculator!");
+
+            Console.ReadKey();
+        }
+    }
+}
